09581027f6667004a5ec516976014beff6cbd0fb
kajuga <kajuga@mail.ru>
2019-06-20 22:21:28 +0300

    p. 126 / p. 382

---
+++ src/chapter_2/SwitchCase.java
@@ -0,0 +1,10 @@
+package chapter_2;
+
+public class SwitchCase {
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/chapter_2/employee/WaterFiller.java
@@ -0,0 +1,5 @@
+package chapter_2.employee;
+
+import chapter_2.Water;
+
+public class WaterFiller {  Water water;}
