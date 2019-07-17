4b4f0c0ba51d031c93153f50e5db56fce9d3af2c
kajuga <kajuga@mail.ru>
2019-07-17 19:35:25 +0300

    17/07/19 p170

---
+++ src/chapter_2/ArrAys.java
@@ -0,0 +1,11 @@
+package chapter_2;
+
+public class ArrAys {
+    public static void main(String[] args) {
+        int[] numbers = new int[10];
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = i + 5;
+        }
+    }
+
+}
---
+++ src/chapter_2/StringBuilderEx.java
@@ -0,0 +1,11 @@
+package chapter_2;
+
+public class StringBuilderEx {
+
+    public static void main(String[] args) {
+        String s = "";
+        System.out.println(s.length());
+    }
+
+
+}
