06e4de2bcdf0e5877d62ddba16767a79da3cf308
kajuga <kajuga@mail.ru>
2019-07-24 22:35:05 +0300

    p. 154 / p. 386

---
+++ src/chapter_2/LocalDateFormatter.java
@@ -0,0 +1,18 @@
+package chapter_2;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
+
+public class LocalDateFormatter {
+    public static void main(String[] args) {
+        LocalDate date = LocalDate.of(2020, Month.JANUARY, 20);
+        LocalTime time = LocalTime.of(11, 12, 34);
+        LocalDateTime dateTime = LocalDateTime.of(date, time);
+        System.out.println(date.format(DateTimeFormatter.ISO_LOCAL_DATE));
+        System.out.println(time.format(DateTimeFormatter.ISO_LOCAL_TIME));
+        System.out.println(dateTime.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
+    }
+}
