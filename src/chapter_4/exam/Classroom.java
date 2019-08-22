a2878921c040d69b8797f608040c04f297cedaad
kajuga <kajuga@mail.ru>
2019-08-22 19:39:05 +0300

    p. 274(до задачи 23) / p. 391 22/08/19

---
+++ src/chapter_4/exam/Classroom.java
@@ -0,0 +1,15 @@
+package chapter_4.exam;
+
+//package my.school;
+public class Classroom {
+
+    private int roomNumber;
+    protected String teacherName;
+    static int globalKey = 54321;
+    public int floor = 3;
+
+    Classroom(int r, String t) {
+        roomNumber = r;
+        teacherName = t;
+    }
+}
