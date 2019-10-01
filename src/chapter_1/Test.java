433f24ce2a016c79b41813985a57d0ceac623690
kajuga <kajuga@mail.ru>
2019-10-01 22:33:00 +0300

    p. 93 пошол по-новой

---
+++ src/chapter_1/Test.java
@@ -0,0 +1,32 @@
+package chapter_1;
+
+public class Test {
+    boolean timeToEat;
+
+
+    public void eatMore(boolean hungry, int amountOfFood) {
+        double x = 39.21;
+        float y = 2.1f;
+        short xx = 10;
+        short yy = 3;
+        short xxx = (short) (xx + yy);
+
+        int numGrains = (int) 5.6;
+        int roomInBelly = 5;
+        int amountEaten = 3;
+        System.out.println(timeToEat);
+        if (hungry) {
+            boolean timeToEat;
+//            System.out.println(timeToEat);
+            while (amountOfFood > 0) {
+                amountEaten = 2;
+                roomInBelly = roomInBelly - amountEaten;
+                amountOfFood = amountOfFood - amountEaten;
+            }
+        }
+        System.out.println(amountOfFood);
+        System.out.println(amountEaten);
+    }
+
+
+}
---
+++ src/chapter_2/SoumExample.java
@@ -0,0 +1,14 @@
+package chapter_2;
+
+public class SoumExample {
+    public static void main(String[] args) {
+        int x = 3;
+        int y = ++x * 5 / x-- + --x;
+        System.out.println("x is " + x);
+        System.out.println("y is " + y);
+
+        long xx = 10;
+        int yy = 5;
+        yy *= xx;  // DOES NOT COMPILE
+    }
+}
