ec7a5d4232e6c0589b4190adb41126fe28c3b847
kajuga <kajuga@mail.ru>
2019-10-04 19:23:27 +0300

    04/10/19 пошол по-новой

---
+++ src/chapter_2/forTest.java
@@ -0,0 +1,15 @@
+package chapter_2;
+
+public class forTest {
+
+    public static void main(String[] args) {
+
+    int x = 0;
+    for(long y = 0, z = 4; x < 5 && y < 10; x++, y++) {
+        System.out.print(y + " ");
+    }
+    System.out.println(" ");
+    System.out.print(x);
+}
+}
+
